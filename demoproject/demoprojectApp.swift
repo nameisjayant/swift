c7e3f9a0e9959af94a57c81129c1507dbe195330
jayant <jayntkumar99@gmail.com>
2022-01-11 19:12:29 +0530

    first commit

---
+++ demoproject/demoprojectApp.swift
@@ -0,0 +1,17 @@
+//
+//  demoprojectApp.swift
+//  demoproject
+//
+//  Created by jayant kumar on 1/11/22.
+//
+
+import SwiftUI
+
+@main
+struct demoprojectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
